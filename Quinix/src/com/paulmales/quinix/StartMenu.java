b3d903e47047a194efb299d349a542814797b92c
MCRocks999 <minecraftrocks999@gmail.com>
2015-07-12 21:48:54 +0100

    Quinix Project - Start Menu Modifications

--- Quinix/src/com/paulmales/quinix/StartMenu.java
+++ Quinix/src/com/paulmales/quinix/StartMenu.java
@@ -6,12 +6,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
 
 @SuppressWarnings("serial")
 public class StartMenu extends JFrame {
 
 	private JPanel contentPane;
 
+	@SuppressWarnings("rawtypes")
 	public StartMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 600);
@@ -21,10 +31,77 @@ public class StartMenu extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 122, 215));
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
+		JPanel viewPort = new JPanel();
+		viewPort.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		viewPort.setBackground(new Color(0, 122, 215));
+		contentPane.add(viewPort, BorderLayout.CENTER);
+		viewPort.setLayout(null);
+		
+		JLabel lblQuinixV = new JLabel("Quinix v1.0.0");
+		lblQuinixV.setForeground(Color.WHITE);
+		lblQuinixV.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
+		lblQuinixV.setHorizontalAlignment(SwingConstants.CENTER);
+		lblQuinixV.setBounds(10, 10, 430, 50);
+		viewPort.add(lblQuinixV);
+		
+		JPanel programListContainer = new JPanel();
+		programListContainer.setBounds(20, 70, 200, 410);
+		viewPort.add(programListContainer);
+		programListContainer.setLayout(new BorderLayout(0, 0));
+		
+		JList programList = new JList();
+		programList.setBackground(new Color(8, 148, 255));
+		programListContainer.add(programList);
+		
+		JButton btnShutdown = new JButton("Shutdown");
+		btnShutdown.setForeground(Color.WHITE);
+		btnShutdown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnShutdown.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
+		btnShutdown.setBounds(20, 549, 420, 40);
+		btnShutdown.setBackground(new Color(34, 157, 252));
+		viewPort.add(btnShutdown);
+		
+		JButton btnSettings = new JButton("Settings");
+		btnSettings.setForeground(Color.WHITE);
+		btnSettings.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
+		btnSettings.setBounds(20, 499, 420, 40);
+		btnSettings.setBackground(new Color(34, 157, 252));
+		viewPort.add(btnSettings);
+		
+		JButton btnFileBrowser = new JButton("File Browser");
+		btnFileBrowser.setForeground(Color.WHITE);
+		btnFileBrowser.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		btnFileBrowser.setBounds(290, 71, 150, 40);
+		btnFileBrowser.setBackground(new Color(0, 70, 123));
+		viewPort.add(btnFileBrowser);
+		
+		JButton btnInternet = new JButton("Internet");
+		btnInternet.setForeground(Color.WHITE);
+		btnInternet.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		btnInternet.setBounds(290, 122, 150, 40);
+		btnInternet.setBackground(new Color(0, 89, 157));
+		viewPort.add(btnInternet);
+		
+		JButton btnVideoPlayer = new JButton("Video Player");
+		btnVideoPlayer.setForeground(Color.WHITE);
+		btnVideoPlayer.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		btnVideoPlayer.setBackground(new Color(0, 70, 123));
+		btnVideoPlayer.setBounds(290, 173, 150, 40);
+		viewPort.add(btnVideoPlayer);
+		
+		JButton btnAudioPlayer = new JButton("Audio Player");
+		btnAudioPlayer.setForeground(Color.WHITE);
+		btnAudioPlayer.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		btnAudioPlayer.setBackground(new Color(0, 89, 157));
+		btnAudioPlayer.setBounds(290, 225, 150, 40);
+		viewPort.add(btnAudioPlayer);
+		
+		JLabel label = new JLabel(new ImageIcon("QuinixData\\file-manager-50.png"));
+		label.setBounds(230, 70, 40, 40);
+		viewPort.add(label);
 	}
-
 }
